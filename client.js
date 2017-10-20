ff7ee446e34db62d1675638ed06bb4325647968a
Mike Bild <mike.bild@googlemail.com>
2017-10-20 14:52:06 +0200

    Upgrade dependencies

--- client.js
+++ client.js
@@ -1,33 +1,57 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { match, Router, browserHistory } from 'react-router'
-import { CookiesProvider } from 'react-cookie'
-import routes from './routes'
+import React from "react";
+import { hydrate } from "react-dom";
+import { match, Router, browserHistory } from "react-router";
+import { CookiesProvider } from "react-cookie";
+import routes from "./routes";
 
-const { pathname, search, hash } = window.location
-const location = `${pathname}${search}${hash}`
+const { pathname, search, hash } = window.location;
+const location = `${pathname}${search}${hash}`;
 
 match({ routes, location }, () => {
-  render(<CookiesProvider><Router routes={routes} history={browserHistory} createElement={handleCreateElement}/></CookiesProvider>, document.getElementById('app'))
-})
+  hydrate(
+    <CookiesProvider>
+      <Router
+        routes={routes}
+        history={browserHistory}
+        createElement={handleCreateElement}
+      />
+    </CookiesProvider>,
+    document.getElementById("app")
+  );
+});
 
-function handleCreateElement (Component, props) {
-  let componentInitialPropsData = {}
+function handleCreateElement(Component, props) {
+  let componentInitialPropsData = {};
   try {
-    const initialPropsData = JSON.parse(document.getElementById('initialPropsData').textContent)
-    componentInitialPropsData = (initialPropsData.find(x => x.name === Component.name || x.name === Component.displayName) || {}).componentInitialPropsData
-  } catch(e) {
-    componentInitialPropsData = {error: e}
+    const initialPropsData = JSON.parse(
+      document.getElementById("initialPropsData").textContent
+    );
+    componentInitialPropsData = (initialPropsData.find(
+      x => x.name === Component.name || x.name === Component.displayName
+    ) || {}
+    ).componentInitialPropsData;
+  } catch (e) {
+    componentInitialPropsData = { error: e };
   }
-  return <Component {...componentInitialPropsData} {...props} />
+  return <Component {...componentInitialPropsData} {...props} />;
 }
 
-if(process.env.NODE_ENV !== 'production') {
-  console.log('Development environment: ServiceWorker is disabled.')
-} else if (!('serviceWorker' in navigator)) {
-  console.error('ServiceWorker is not supported.')
+if (process.env.NODE_ENV !== "production") {
+  console.log("Development environment: ServiceWorker is disabled.");
+} else if (!("serviceWorker" in navigator)) {
+  console.error("ServiceWorker is not supported.");
 } else {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js')
-  .then(registration => console.log('ServiceWorker registration successful with scope: ', registration.scope), err => console.log('ServiceWorker registration failed: ', err))
-  .catch(console.error))
+  window.addEventListener("load", () =>
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(
+        registration =>
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          ),
+        err => console.log("ServiceWorker registration failed: ", err)
+      )
+      .catch(console.error)
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,93 @@
-process.noDeprecation = true
-const NODE_ENV = process.env.NODE_ENV
-const IS_PROD = NODE_ENV === 'production'
-const path = require('path')
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+process.noDeprecation = true;
+const NODE_ENV = process.env.NODE_ENV;
+const IS_PROD = NODE_ENV === "production";
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
-  entry: ['babel-polyfill', 'isomorphic-fetch', path.join(__dirname, 'client.js')],
-  output: {path: `${process.cwd()}/static`, publicPath: '/', strictModuleExceptionHandling: false},
+  entry: [
+    "babel-polyfill",
+    "isomorphic-fetch",
+    path.join(__dirname, "client.js")
+  ],
+  output: {
+    path: `${process.cwd()}/static`,
+    publicPath: "/",
+    strictModuleExceptionHandling: false
+  },
   node: {
     __filename: true,
     __dirname: true,
-    path: 'empty',
-    fs: 'empty',
-    child_process: 'empty',
-    vm: 'empty',
-    net: 'empty',
-    dns: 'empty',
+    path: "empty",
+    fs: "empty",
+    child_process: "empty",
+    vm: "empty",
+    net: "empty",
+    dns: "empty"
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader?cacheDirectory',
+        loader: "babel-loader?cacheDirectory",
         exclude: /node_modules\/(?!(reunify)\/).*/,
         query: {
-          presets: ['env', 'react'],
-          plugins: ['transform-class-properties',  'babel-plugin-styled-components'],
-        },
+          presets: ["env", "react"],
+          plugins: [
+            "transform-class-properties",
+            [
+              "babel-plugin-styled-components",
+              {
+                ssr: true
+              }
+            ]
+          ]
+        }
       },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'}) },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
-    ],
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      { test: /\.json$/, loader: "json-loader" },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       options: {
         babelrc: false,
-        cacheDirectory: true,
-      },
-    }),
-    new webpack.DefinePlugin({
-      'process.BROWSER': JSON.stringify(true),
-      'process.env': {
-        'BROWSER': JSON.stringify(true),
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        cacheDirectory: true
       }
     }),
-    new ExtractTextPlugin('main.css'),
-  ],
-}
-config.devtool = IS_PROD ? 'cheap-module-source-map' : 'cheap-module-eval-source-map'
-if(IS_PROD) config.plugins.push(new webpack.optimize.DedupePlugin())
-if(IS_PROD) config.plugins.push(new webpack.optimize.UglifyJsPlugin({mangle: false}))
-if(IS_PROD) config.plugins.push(new webpack.optimize.AggressiveMergingPlugin())
+    new webpack.DefinePlugin({
+      "process.BROWSER": JSON.stringify(true),
+      "process.env": {
+        BROWSER: JSON.stringify(true),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new ExtractTextPlugin("main.css")
+  ]
+};
+config.devtool = IS_PROD
+  ? "cheap-module-source-map"
+  : "cheap-module-eval-source-map";
+if (IS_PROD) config.plugins.push(new webpack.optimize.DedupePlugin());
+if (IS_PROD)
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({ mangle: false }));
+if (IS_PROD)
+  config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 
-if(!IS_PROD) config.plugins.unshift(new webpack.HotModuleReplacementPlugin())
-if(!IS_PROD) config.entry.unshift('webpack-hot-middleware/client?noInfo=true&quiet=true')
-if(!IS_PROD) config.module.loaders[0].query.presets.unshift('react-hmre')
+if (!IS_PROD) config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+if (!IS_PROD)
+  config.entry.unshift("webpack-hot-middleware/client?noInfo=true&quiet=true");
+if (!IS_PROD) config.module.loaders[0].query.presets.unshift("react-hmre");
 
-module.exports = config
+module.exports = config;
